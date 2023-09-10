df25c16ba8fb588324c9ee4250fa8f1d665080d0
Antonio Arbues <avarbues@gmail.com>
2023-09-10 13:21:05 +0000

    mlps bool use layer norm

--- utils/models.py
+++ utils/models.py
@@ -113,7 +113,7 @@ class MLP(nn.Module):
     MLP without activation function at the last layer.
     """
     hidden_dim_layers: Sequence[int]
-    use_layer_norm: bool = True
+    use_layer_norm: bool
 
     def setup(self) -> None:
         if self.use_layer_norm:
@@ -148,14 +148,15 @@ class AgentStateNet(nn.Module):
     # one_hot_num_classes: int
     num_embeddings: int
     loaded_max: int
+    mlp_use_layernorm: bool
     num_embedding_features: int = 8
     hidden_dim_layers_mlp_one_hot: Sequence[int] = (16, 32)
     hidden_dim_layers_mlp_continuous: Sequence[int] = (16, 32)
     
     def setup(self) -> None:
         self.embedding = nn.Embed(num_embeddings=self.num_embeddings, features=self.num_embedding_features)
-        self.mlp_one_hot = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp_one_hot)
-        self.mlp_continuous = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp_continuous)
+        self.mlp_one_hot = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp_one_hot, use_layer_norm=self.mlp_use_layernorm)
+        self.mlp_continuous = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp_continuous, use_layer_norm=self.mlp_use_layernorm)
     
     def __call__(self, obs: dict[str, Array]):
         # NOTE: this implementation uses the pos of the agent explicitly
@@ -181,6 +182,7 @@ class LocalMapNet(nn.Module):
     Pre-process one or multiple maps.
     """
     map_min_max: Sequence[int]
+    mlp_use_layernorm: bool
     hidden_dim_layers_mlp: Sequence[int] = (128, 16)
 
     def setup(self) -> None:
@@ -192,7 +194,7 @@ class LocalMapNet(nn.Module):
         #                 n_classes=16,
         #                 # norm_cls=nn.LayerNorm,
         #             )
-        self.mlp = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp)
+        self.mlp = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp, use_layer_norm=self.mlp_use_layernorm)
 
     def __call__(self, obs: dict[str, Array]):
         x_action = normalize(obs[1], self.map_min_max[0], self.map_min_max[1])
@@ -458,33 +460,9 @@ class MapsNet(nn.Module):
         # x = self.conv1(x)
         x = self.cnn(x)
         return x
+
     
-
-# class SimplifiedMapsNet(nn.Module):
-#     """
-#     Pre-process one or multiple maps.
-#     """
-#     map_min_max: Sequence[int] = (-2, 2)  # TODO from config
-#     hidden_dim_layers_mlp: Sequence[int] = (128, 32)
-
-#     def setup(self) -> None:
-#         self.mlp = MLP(hidden_dim_layers=self.hidden_dim_layers_mlp)
-
-#     def __call__(self, obs: dict[str, Array]):
-#         action_map = normalize(obs[3], self.map_min_max[0], self.map_min_max[1])
-
-#         # delta_target_map = jnp.clip(obs[3] - obs[2], a_max=0)
-#         # delta_target_map = normalize(delta_target_map, self.map_min_max[0], 0)
-
-#         target_map = normalize(obs[4], self.map_min_max[0], self.map_min_max[1])
-
-#         x = jnp.concatenate((action_map[..., None], target_map[..., None], obs[5][..., None]), axis=-1)
-#         # x = self.conv1(x)
-#         x = self.mlp(x.reshape(x.shape[0], -1))
-#         return x
-
-
-# class CategoricalNet(nn.Module):
+# class SimplifiedDecoupledCategoricalNet(nn.Module):
 #     """
 #     The full net.
 
@@ -496,207 +474,88 @@ class MapsNet(nn.Module):
 #     4 - obs["target_map"],
 #     5 - obs["traversability_mask"]
 #     """
+#     use_action_masking: bool
+#     mask_out_arm_extension: bool
+#     num_embeddings_agent: int
+#     map_min_max: Sequence[int]
+#     local_map_min_max: Sequence[int]
+#     loaded_max: int
 #     action_type: Union[TrackedAction, WheeledAction] = TrackedAction
-#     hidden_dim_layers_common: Sequence[int] = (64, 32)
-#     hidden_dim_pi: Sequence[int] = (16, 16)
-#     hidden_dim_v: Sequence[int] = (16, 4, 1)
+#     # hidden_dim_layers_common: Sequence[int] = (256, 64)
+#     hidden_dim_pi: Sequence[int] = (128, 32)
+#     hidden_dim_v: Sequence[int] = (128, 32, 1)
 
 #     def setup(self) -> None:
 #         num_actions = self.action_type.get_num_actions()
 
-#         self.common_mlp = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
+#         # self.common_mlp_v = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
+#         # self.common_mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
+
 #         self.mlp_v = MLP(hidden_dim_layers=self.hidden_dim_v)
 #         self.mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_pi + (num_actions,))
 
-#         self.agent_state_net = AgentStateNet()
-#         self.maps_net = MapsNet()
-#         self.local_map_net = LocalMapNet()
+#         self.local_map_net_v = LocalMapNet(map_min_max=self.local_map_min_max)
+#         self.local_map_net_pi = LocalMapNet(map_min_max=self.local_map_min_max)
+
+#         self.agent_state_net_v = AgentStateNet(num_embeddings=self.num_embeddings_agent, loaded_max=self.loaded_max)
+#         self.agent_state_net_pi = AgentStateNet(num_embeddings=self.num_embeddings_agent, loaded_max=self.loaded_max)
+
+#         # Resnet
+#         self.maps_net_v = MapsNet(self.map_min_max)
+#         self.maps_net_pi = MapsNet(self.map_min_max)
+#         # MLP for maps
+#         # self.maps_net_v = SimplifiedMapsNet()
+#         # self.maps_net_pi = SimplifiedMapsNet()
 
 #         self.activation = nn.relu
 
 #     def __call__(self, obs: Array, action_mask: Array) -> Array:
-#         x_agent_state = self.agent_state_net(obs)
-#         x_maps = self.maps_net(obs)
-#         x_local_map = self.local_map_net(obs)
+#         x_agent_state_v = self.agent_state_net_v(obs)
+#         x_agent_state_pi = self.agent_state_net_pi(obs)
+        
+#         x_maps_v = self.maps_net_v(obs)
+#         x_maps_pi = self.maps_net_pi(obs)
 
-#         x = jnp.concatenate((x_agent_state, x_local_map, x_maps), axis=-1)
-#         x = self.activation(x)
+#         x_local_map_v = self.local_map_net_v(obs)
+#         x_local_map_pi = self.local_map_net_pi(obs)
 
-#         x = self.common_mlp(x)
-#         x = self.activation(x)
+#         xv = jnp.concatenate((x_agent_state_v, x_maps_v, x_local_map_v), axis=-1)
+#         xv = self.activation(xv)
 
-#         v = self.mlp_v(x)
+#         xpi = jnp.concatenate((x_agent_state_pi, x_maps_pi, x_local_map_pi), axis=-1)
+#         xpi = self.activation(xpi)
 
-#         x_pi = self.mlp_pi(x)
-#         # x_pi = jnp.where(
-#         #     action_mask.astype(jnp.bool_),
-#         #     x_pi,
-#         #     -1e8
-#         # )
+#         # xv = self.common_mlp_v(xv)
+#         # xv = self.activation(xv)
 
-#         # # Mask out arm extension
-#         x_pi = x_pi.at[..., -2].set(-1e8)
-#         x_pi = x_pi.at[..., -3].set(-1e8)
+#         # xpi = self.common_mlp_pi(xpi)
+#         # xpi = self.activation(xpi)
+
+#         v = self.mlp_v(xv)
+
+#         xpi = self.mlp_pi(xpi)
+
+#         # INVALID ACTION MASKING
+#         if self.use_action_masking:
+#             action_mask = action_mask.astype(jnp.bool_)
+#             # OPTION 1
+#             xpi = xpi * action_mask - 1e8 * (~action_mask)
+#             # OPTION 2
+#             # xpi = jnp.where(
+#             #     action_mask,
+#             #     xpi,
+#             #     -1e8
+#             # )
+
+#         if self.mask_out_arm_extension:
+#             xpi = xpi.at[..., -2].set(-1e8)
+#             xpi = xpi.at[..., -3].set(-1e8)
 
 #         # jax.debug.print("action_mask={x}", x=action_mask)
-#         # jax.debug.print("x_pi={x}", x=x_pi)
+#         # jax.debug.print("xpi={x}", x=xpi)
         
-#         pi = tfp.distributions.Categorical(logits=x_pi)
-#         return v, pi
-
-
-# class SimplifiedCategoricalNet(nn.Module):
-#     """
-#     The full net.
-
-#     The obs List follows the following order:
-#     0 - obs["agent_state"],
-#     1 - obs["local_map_action"],
-#     2 - obs["local_map_target"],
-#     3 - obs["action_map"],
-#     4 - obs["target_map"],
-#     5 - obs["traversability_mask"]
-#     """
-#     action_type: Union[TrackedAction, WheeledAction] = TrackedAction
-#     hidden_dim_layers_common: Sequence[int] = (64, 32)
-#     hidden_dim_pi: Sequence[int] = (16, 16)
-#     hidden_dim_v: Sequence[int] = (16, 4, 1)
-
-#     def setup(self) -> None:
-#         num_actions = self.action_type.get_num_actions()
-
-#         self.common_mlp = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
-#         self.mlp_v = MLP(hidden_dim_layers=self.hidden_dim_v)
-#         self.mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_pi + (num_actions,))
-
-#         self.agent_state_net = AgentStateNet()
-#         self.maps_net = SimplifiedMapsNet()
-
-#         self.activation = nn.relu
-
-#     def __call__(self, obs: Array, action_mask: Array) -> Array:
-#         x_agent_state = self.agent_state_net(obs)
-#         x_maps = self.maps_net(obs)
-
-#         x = jnp.concatenate((x_agent_state, x_maps), axis=-1)
-#         x = self.activation(x)
-
-#         x = self.common_mlp(x)
-#         x = self.activation(x)
-
-#         v = self.mlp_v(x)
-
-#         x_pi = self.mlp_pi(x)
-#         # x_pi = jnp.where(
-#         #     action_mask.astype(jnp.bool_),
-#         #     x_pi,
-#         #     -1e8
-#         # )
-
-#         # # Mask out arm extension
-#         x_pi = x_pi.at[..., -2].set(-1e8)
-#         x_pi = x_pi.at[..., -3].set(-1e8)
-
-#         # jax.debug.print("action_mask={x}", x=action_mask)
-#         # jax.debug.print("x_pi={x}", x=x_pi)
-        
-#         pi = tfp.distributions.Categorical(logits=x_pi)
-#         return v, pi
-    
-class SimplifiedDecoupledCategoricalNet(nn.Module):
-    """
-    The full net.
-
-    The obs List follows the following order:
-    0 - obs["agent_state"],
-    1 - obs["local_map_action"],
-    2 - obs["local_map_target"],
-    3 - obs["action_map"],
-    4 - obs["target_map"],
-    5 - obs["traversability_mask"]
-    """
-    use_action_masking: bool
-    mask_out_arm_extension: bool
-    num_embeddings_agent: int
-    map_min_max: Sequence[int]
-    local_map_min_max: Sequence[int]
-    loaded_max: int
-    action_type: Union[TrackedAction, WheeledAction] = TrackedAction
-    # hidden_dim_layers_common: Sequence[int] = (256, 64)
-    hidden_dim_pi: Sequence[int] = (128, 32)
-    hidden_dim_v: Sequence[int] = (128, 32, 1)
-
-    def setup(self) -> None:
-        num_actions = self.action_type.get_num_actions()
-
-        # self.common_mlp_v = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
-        # self.common_mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
-
-        self.mlp_v = MLP(hidden_dim_layers=self.hidden_dim_v)
-        self.mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_pi + (num_actions,))
-
-        self.local_map_net_v = LocalMapNet(map_min_max=self.local_map_min_max)
-        self.local_map_net_pi = LocalMapNet(map_min_max=self.local_map_min_max)
-
-        self.agent_state_net_v = AgentStateNet(num_embeddings=self.num_embeddings_agent, loaded_max=self.loaded_max)
-        self.agent_state_net_pi = AgentStateNet(num_embeddings=self.num_embeddings_agent, loaded_max=self.loaded_max)
-
-        # Resnet
-        self.maps_net_v = MapsNet(self.map_min_max)
-        self.maps_net_pi = MapsNet(self.map_min_max)
-        # MLP for maps
-        # self.maps_net_v = SimplifiedMapsNet()
-        # self.maps_net_pi = SimplifiedMapsNet()
-
-        self.activation = nn.relu
-
-    def __call__(self, obs: Array, action_mask: Array) -> Array:
-        x_agent_state_v = self.agent_state_net_v(obs)
-        x_agent_state_pi = self.agent_state_net_pi(obs)
-        
-        x_maps_v = self.maps_net_v(obs)
-        x_maps_pi = self.maps_net_pi(obs)
-
-        x_local_map_v = self.local_map_net_v(obs)
-        x_local_map_pi = self.local_map_net_pi(obs)
-
-        xv = jnp.concatenate((x_agent_state_v, x_maps_v, x_local_map_v), axis=-1)
-        xv = self.activation(xv)
-
-        xpi = jnp.concatenate((x_agent_state_pi, x_maps_pi, x_local_map_pi), axis=-1)
-        xpi = self.activation(xpi)
-
-        # xv = self.common_mlp_v(xv)
-        # xv = self.activation(xv)
-
-        # xpi = self.common_mlp_pi(xpi)
-        # xpi = self.activation(xpi)
-
-        v = self.mlp_v(xv)
-
-        xpi = self.mlp_pi(xpi)
-
-        # INVALID ACTION MASKING
-        if self.use_action_masking:
-            action_mask = action_mask.astype(jnp.bool_)
-            # OPTION 1
-            xpi = xpi * action_mask - 1e8 * (~action_mask)
-            # OPTION 2
-            # xpi = jnp.where(
-            #     action_mask,
-            #     xpi,
-            #     -1e8
-            # )
-
-        if self.mask_out_arm_extension:
-            xpi = xpi.at[..., -2].set(-1e8)
-            xpi = xpi.at[..., -3].set(-1e8)
-
-        # jax.debug.print("action_mask={x}", x=action_mask)
-        # jax.debug.print("xpi={x}", x=xpi)
-        
-        # Note: returning logits xpi, not distribution pi!
-        return v, xpi
+#         # Note: returning logits xpi, not distribution pi!
+#         return v, xpi
     
 
 class SimplifiedCoupledCategoricalNet(nn.Module):
@@ -721,6 +580,7 @@ class SimplifiedCoupledCategoricalNet(nn.Module):
     # hidden_dim_layers_common: Sequence[int] = (256, 64)
     hidden_dim_pi: Sequence[int] = (128, 32)
     hidden_dim_v: Sequence[int] = (128, 32, 1)
+    mlp_use_layernorm: bool = False
 
     def setup(self) -> None:
         num_actions = self.action_type.get_num_actions()
@@ -728,12 +588,12 @@ class SimplifiedCoupledCategoricalNet(nn.Module):
         # self.common_mlp_v = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
         # self.common_mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_layers_common)
 
-        self.mlp_v = MLP(hidden_dim_layers=self.hidden_dim_v)
-        self.mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_pi + (num_actions,))
+        self.mlp_v = MLP(hidden_dim_layers=self.hidden_dim_v, use_layer_norm=self.mlp_use_layernorm)
+        self.mlp_pi = MLP(hidden_dim_layers=self.hidden_dim_pi + (num_actions,), use_layer_norm=self.mlp_use_layernorm)
 
-        self.local_map_net = LocalMapNet(map_min_max=self.local_map_min_max)
+        self.local_map_net = LocalMapNet(map_min_max=self.local_map_min_max, mlp_use_layernorm=self.mlp_use_layernorm)
 
-        self.agent_state_net = AgentStateNet(num_embeddings=self.num_embeddings_agent, loaded_max=self.loaded_max)
+        self.agent_state_net = AgentStateNet(num_embeddings=self.num_embeddings_agent, loaded_max=self.loaded_max, mlp_use_layernorm=self.mlp_use_layernorm)
 
         # Resnet
         self.maps_net = MapsNet(self.map_min_max)
@@ -778,141 +638,3 @@ class SimplifiedCoupledCategoricalNet(nn.Module):
         
         # Note: returning logits xpi, not distribution pi!
         return v, xpi
-
-
-# class CategoricalSeparateMLP(nn.Module):
-#     """Split Actor-Critic Architecture for PPO."""
-
-#     num_output_units: int
-#     num_hidden_units: int
-#     num_hidden_layers: int
-#     prefix_actor: str = "actor"
-#     prefix_critic: str = "critic"
-#     model_name: str = "separate-mlp"
-#     flatten_2d: bool = False  # Catch case
-#     flatten_3d: bool = False  # Rooms/minatar case
-
-#     @nn.compact
-#     def __call__(self, x, rng):
-#         # Flatten a single 2D image
-#         if self.flatten_2d and len(x.shape) == 2:
-#             x = x.reshape(-1)
-#         # Flatten a batch of 2d images into a batch of flat vectors
-#         if self.flatten_2d and len(x.shape) > 2:
-#             x = x.reshape(x.shape[0], -1)
-
-#         # Flatten a single 3D image
-#         if self.flatten_3d and len(x.shape) == 3:
-#             x = x.reshape(-1)
-#         # Flatten a batch of 3d images into a batch of flat vectors
-#         if self.flatten_3d and len(x.shape) > 3:
-#             x = x.reshape(x.shape[0], -1)
-#         x_v = nn.relu(
-#             nn.Dense(
-#                 self.num_hidden_units,
-#                 name=self.prefix_critic + "_fc_1",
-#                 bias_init=default_mlp_init(),
-#             )(x)
-#         )
-#         # Loop over rest of intermediate hidden layers
-#         for i in range(1, self.num_hidden_layers):
-#             x_v = nn.relu(
-#                 nn.Dense(
-#                     self.num_hidden_units,
-#                     name=self.prefix_critic + f"_fc_{i+1}",
-#                     bias_init=default_mlp_init(),
-#                 )(x_v)
-#             )
-#         v = nn.Dense(
-#             1,
-#             name=self.prefix_critic + "_fc_v",
-#             bias_init=default_mlp_init(),
-#         )(x_v)
-
-#         x_a = nn.relu(
-#             nn.Dense(
-#                 self.num_hidden_units,
-#                 bias_init=default_mlp_init(),
-#             )(x)
-#         )
-#         # Loop over rest of intermediate hidden layers
-#         for i in range(1, self.num_hidden_layers):
-#             x_a = nn.relu(
-#                 nn.Dense(
-#                     self.num_hidden_units,
-#                     bias_init=default_mlp_init(),
-#                 )(x_a)
-#             )
-#         logits = nn.Dense(
-#             self.num_output_units,
-#             bias_init=default_mlp_init(),
-#         )(x_a)
-#         # pi = distrax.Categorical(logits=logits)
-#         pi = tfp.distributions.Categorical(logits=logits)
-#         return v, pi
-
-
-# class GaussianSeparateMLP(nn.Module):
-#     """Split Actor-Critic Architecture for PPO."""
-
-#     num_output_units: int
-#     num_hidden_units: int
-#     num_hidden_layers: int
-#     prefix_actor: str = "actor"
-#     prefix_critic: str = "critic"
-#     min_std: float = 0.001
-#     model_name: str = "separate-mlp"
-
-#     @nn.compact
-#     def __call__(self, x, rng):
-#         x_v = nn.relu(
-#             nn.Dense(
-#                 self.num_hidden_units,
-#                 name=self.prefix_critic + "_fc_1",
-#                 bias_init=default_mlp_init(),
-#             )(x)
-#         )
-#         # Loop over rest of intermediate hidden layers
-#         for i in range(1, self.num_hidden_layers):
-#             x_v = nn.relu(
-#                 nn.Dense(
-#                     self.num_hidden_units,
-#                     name=self.prefix_critic + f"_fc_{i+1}",
-#                     bias_init=default_mlp_init(),
-#                 )(x_v)
-#             )
-#         v = nn.Dense(
-#             1,
-#             name=self.prefix_critic + "_fc_v",
-#             bias_init=default_mlp_init(),
-#         )(x_v)
-
-#         x_a = nn.relu(
-#             nn.Dense(
-#                 self.num_hidden_units,
-#                 name=self.prefix_actor + "_fc_1",
-#                 bias_init=default_mlp_init(),
-#             )(x)
-#         )
-#         # Loop over rest of intermediate hidden layers
-#         for i in range(1, self.num_hidden_layers):
-#             x_a = nn.relu(
-#                 nn.Dense(
-#                     self.num_hidden_units,
-#                     name=self.prefix_actor + f"_fc_{i+1}",
-#                     bias_init=default_mlp_init(),
-#                 )(x_a)
-#             )
-#         mu = nn.Dense(
-#             self.num_output_units,
-#             name=self.prefix_actor + "_fc_mu",
-#             bias_init=default_mlp_init(),
-#         )(x_a)
-#         log_scale = nn.Dense(
-#             self.num_output_units,
-#             name=self.prefix_actor + "_fc_scale",
-#             bias_init=default_mlp_init(),
-#         )(x_a)
-#         scale = jax.nn.softplus(log_scale) + self.min_std
-#         pi = tfp.distributions.MultivariateNormalDiag(mu, scale)
-#         return v, pi
