64a4b9b4b0c3b91ebf0d6d902613deb4e413469a
Antonio Arbues <avarbues@gmail.com>
2023-09-12 11:45:45 +0200

    obs preprocessing in visualize.py

--- visualize.py
+++ visualize.py
@@ -9,7 +9,7 @@ from utils.models import get_model_ready
 from utils.helpers import load_pkl_object
 from terra.env import TerraEnvBatch
 import jax.numpy as jnp
-from utils.ppo import obs_to_model_input, wrap_action
+from utils.ppo import obs_to_model_input, wrap_action, clip_action_maps_in_obs, cut_local_map_layers
 from terra.state import State
 import matplotlib.animation as animation
 from utils.curriculum import Curriculum
@@ -44,6 +44,10 @@ def rollout_episode(env: TerraEnvBatch, model, model_params, env_cfgs, force_res
         obs_seq = _append_to_obs(obs, obs_seq)
         rng, rng_act, rng_step = jax.random.split(rng, 3)
         if model is not None:
+            if rl_config["clip_action_maps"]:
+                obs = clip_action_maps_in_obs(obs)
+            if rl_config["mask_out_arm_extension"]:
+                obs = cut_local_map_layers(obs)
             obs = obs_to_model_input(obs)
             action_mask = jnp.ones((8,), dtype=jnp.bool_)  # TODO implement action masking
             v, logits_pi = model.apply(model_params, obs, action_mask)
