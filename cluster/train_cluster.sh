0c455e93889ba319426c968c02d14326595412bd
Stanislaw Piasecki <spiasecki@eu-login-28.euler.ethz.ch>
2025-03-21 11:18:48 +0100

    Add my Euler config

--- cluster/train_cluster.sh
+++ cluster/train_cluster.sh
@@ -14,20 +14,20 @@ module load eth_proxy
 module load stack/2024-06 cuda/12.1.1
 
 # Set paths to conda
-CONDA_ROOT=/cluster/home/lterenzi/miniconda3
-CONDA_ENV=terra                 # Your conda environment name
+CONDA_ROOT=/cluster/home/spiasecki/miniconda3
+CONDA_ENV=terra
 
 # Activate conda environment properly for batch jobs
 eval "$($CONDA_ROOT/bin/conda shell.bash hook)"
 conda activate $CONDA_ENV
 
 # Set environment variables and run training
-export DATASET_PATH=/cluster/home/lterenzi/terra_jax/terra/data/terra/train
-export DATASET_SIZE=200
+export DATASET_PATH=/cluster/home/spiasecki/terra/data/
+export DATASET_SIZE=500
 
 # Add the terra package to the Python path
 export PYTHONPATH=$PYTHONPATH:/cluster/home/lterenzi/terra_jax
 
 # Change to the directory containing train.py or use the full path
-cd /cluster/home/lterenzi/terra_jax/terra-baselines
+cd /cluster/home/spiasecki/terra-baselines
 python train.py
