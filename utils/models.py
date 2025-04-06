e333e273861140f5bf9df09934a16217952cc351
Stachq1 <spiasecki@student.ethz.ch>
2025-04-06 12:26:34 +0200

    Make the CNN even bigger

--- utils/models.py
+++ utils/models.py
@@ -211,15 +211,15 @@ class AtariCNN(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(features=16, kernel_size=(8, 8), strides=(4, 4))(x)
+        x = nn.Conv(features=32, kernel_size=(8, 8), strides=(4, 4))(x)
         x = nn.relu(x)
         x = nn.Conv(features=32, kernel_size=(4, 4), strides=(2, 2))(x)
         x = nn.relu(x)
-        x = nn.Conv(features=32, kernel_size=(3, 3), strides=(1, 1))(x)
+        x = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1))(x)
         x = nn.relu(x)
         x = x.reshape((x.shape[0], -1))
 
-        x = nn.Dense(features=128)(x)
+        x = nn.Dense(features=256)(x)
         x = nn.relu(x)
         x = nn.Dense(features=32)(x)
         return x
