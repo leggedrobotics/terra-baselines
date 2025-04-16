657bc6e133b0f8e01b17bc82bc9313ba53e9397d
Stachq1 <spiasecki@student.ethz.ch>
2025-04-16 14:50:05 +0200

    Change cluster training config

--- cluster/train_cluster.sh
+++ cluster/train_cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --gpus=rtx_3090:1
-#SBATCH --time=3:00:00
+#SBATCH --gpus=rtx_4090:2
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=4048
 #SBATCH --mail-type=END
 #SBATCH --mail-user=name@mail
@@ -14,20 +14,20 @@ module load eth_proxy
 module load stack/2024-06 cuda/12.1.1
 
 # Set paths to conda
-CONDA_ROOT=/cluster/home/lterenzi/miniconda3
-CONDA_ENV=terra                 # Your conda environment name
+CONDA_ROOT=/cluster/home/spiasecki/miniconda3
+CONDA_ENV=terra
 
 # Activate conda environment properly for batch jobs
 eval "$($CONDA_ROOT/bin/conda shell.bash hook)"
 conda activate $CONDA_ENV
 
 # Set environment variables and run training
-export DATASET_PATH=/cluster/home/lterenzi/terra_jax/terra/data/terra/train
+export DATASET_PATH=/cluster/home/spiasecki/terra/data/
 export DATASET_SIZE=200
 
 # Add the terra package to the Python path
 export PYTHONPATH=$PYTHONPATH:/cluster/home/lterenzi/terra_jax
 
 # Change to the directory containing train.py or use the full path
-cd /cluster/home/lterenzi/terra_jax/terra-baselines
+cd /cluster/home/spiasecki/terra-baselines
 python train.py
