65ad81d80221383fad0b807f49bc308e90a5cec2
Stachq1 <spiasecki@student.ethz.ch>
2025-05-15 10:03:11 +0200

    Change job hours to 120 from 128

--- cluster/sweep_cluster.sh
+++ cluster/sweep_cluster.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus=rtx_4090:8
-#SBATCH --time=128:00:00
+#SBATCH --time=120:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=name@mail
