dcc928790c9423aed0252362bc4b26c6ca1a6e03
Guru Aathavan <guruaathavan.alu@gmail.com>
2025-06-08 15:01:09 +0200

    cluster train path updates

--- cluster/train_cluster.sh
+++ cluster/train_cluster.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH --cpus-per-task=8
-#SBATCH --gpus=rtx_4090:2
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=rtx_4090:8
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4048
 #SBATCH --mail-type=END
 #SBATCH --mail-user=name@mail
 #SBATCH --job-name="training-$(date +"%Y-%m-%dT%H:%M")"
@@ -14,7 +14,7 @@ module load eth_proxy
 module load stack/2024-06 cuda/12.1.1
 
 # Set paths to conda
-CONDA_ROOT=/cluster/home/spiasecki/miniconda3
+CONDA_ROOT=/cluster/home/galagu/miniconda3
 CONDA_ENV=terra
 
 # Activate conda environment properly for batch jobs
@@ -22,9 +22,12 @@ eval "$($CONDA_ROOT/bin/conda shell.bash hook)"
 conda activate $CONDA_ENV
 
 # Set environment variables and run training
-export DATASET_PATH=/cluster/home/spiasecki/terra/data/
-export DATASET_SIZE=1800
+export DATASET_PATH=/cluster/home/galagu/terra/data1/
+export DATASET_SIZE=200
+
+# Add the terra package to the Python path
+export PYTHONPATH=$PYTHONPATH:/cluster/home/lterenzi/terra_jax
 
 # Change to the directory containing train.py or use the full path
-cd /cluster/home/spiasecki/terra-baselines
+cd /cluster/home/galagu/terra-baselines
 python train.py
